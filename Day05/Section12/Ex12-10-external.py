bf47417b0046bd93d78fd3f5dd891e9901b3e7eb
bluemin <zeem_m2@naver.com>
2022-12-04 11:41:19 +0900

    Day 06 로또 번호 추첨 프로그램 practice!

---
+++ Day05/Section12/Ex12-10-external.py
@@ -0,0 +1,37 @@
+'''
+패키지
+    모듈 상위 개념
+    모듈의 집합을 의미
+
+패키지 설치
+console> pip install package명
+
+패키지 삭제
+console> pip uninstall package명
+
+설치 x -> 예제 주석처리함
+'''
+# import numpy as np
+#
+# print(np.sum([1,2,3,4,5]))
+#
+# a = np.array([1,2,3])
+# b = np.array([4,5,6])
+#
+# # 각 요소 더하기
+# c = a + b
+# c = np.add(a, b)
+# print(c)
+#
+# # 각 요소 빼기
+# c = a - b
+# c = np.substract(a, b)
+# print(c)
+#
+# # 각 요소 곱하기
+# c = a * b
+# c = np.(a,b)
+#
+# # 각 요소 나누기
+# c = a / b
+# c = np.
